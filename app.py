5bbdf13d8629bf73ab2cadbeba7d93f94811d805
ry <yeryanyin@gmail.com>
2026-02-05 09:25:34 -0500

    only twice

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 random = range(20)
 guess = input("Guess a number from 0 to 20")
-if guess == random:
-    print("You were correct")
-    print[guess]
-else:
-    print("Wrong guess again")
+history = int(guess)
+for i in guess:
+    if guess == random:
+        print("You were correct")
+        quit()
+    else:
+        print("Wrong guess")
+        print(guess)
+        guess = input("Guess a number from 0 to 20")
