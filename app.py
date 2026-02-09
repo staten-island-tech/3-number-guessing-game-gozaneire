39cdedfce721d08fad114403e93dd1f4c3c690b6
ry <yeryanyin@gmail.com>
2026-02-09 09:22:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-random = range(20)
-guess = input("Guess a number from 0 to 20")
-history = int(guess)
-for i in guess:
-    if guess == random:
+import random
+number = random.randint(0,1000)
+
+guess = int(input("Guess number"))
+for i in number:
+    if guess == number:
         print("You were correct")
         quit()
     else:
         print("Wrong guess")
         print(guess)
-        guess = input("Guess a number from 0 to 20")
