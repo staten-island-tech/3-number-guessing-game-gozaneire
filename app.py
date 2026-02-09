aa27b856d0022b40e4d3c06767f96da17bf568c8
ry <yeryanyin@gmail.com>
2026-02-09 09:25:32 -0500

    Higher and lower

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import random
-number = random.randint(0,1000)
+number = random.randint(0,100)
 
 guess = int(input("Guess number"))
-for i in number:
-    if guess == number:
-        print("You were correct")
-        quit()
-    else:
-        print("Wrong guess")
-        print(guess)
+
+if guess == number:
+    print("You were correct")
+    quit()
+else:
+    print("Wrong guess")
+    print(guess)
