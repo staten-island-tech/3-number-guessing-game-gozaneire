ad509dee3ef840c2bec36544a0193d7e43dc9470
ry <yeryanyin@gmail.com>
2026-02-09 09:26:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import random
 number = random.randint(0,100)
 
-guess = int(input("Guess number"))
+guess = int(input("Guess number between 0 and 100"))
 
 if guess == number:
     print("You were correct")
     quit()
-else:
-    print("Wrong guess")
-    print(guess)
+elif guess > number:
+    print("The number was lower")
+elif guess < number:
+    print("The number was higher")
