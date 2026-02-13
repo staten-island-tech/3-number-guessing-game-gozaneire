471c5762eeee79baa87bffc184a8edfe0d678a14
ry <yeryanyin@gmail.com>
2026-02-13 09:09:44 -0500

    Twice days

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 import random
 number = random.randint(0,50)
 
+guess_history = []
 guess = int(input("Guess number between 0 and 50"))
-guess_history = " "
-
-def guess_history():
-    return guess
 
 while guess != number:
     if guess > number:
         print(f"The number is lower than {guess}")
-        guess_history =+ guess
-        guess = int(input("Guess again from 0 to 50"))
+        guess_history.append(guess)
+        guess = int(input("Guess number between 0 and 50"))
     if guess < number:
         print(f"The number is greater than {guess}")
-        guess_history =+ guess
-        guess = int(input("Guess again from 0 to 50"))
-
-print("You were correct!")
+        guess_history.append(guess)
+        guess = int(input("Guess number between 0 and 50"))
+    
+print(f"You were correct! The number is {number}.")
 print(guess_history)
