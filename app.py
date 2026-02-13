f4f8f9e099be7ad9f67c2b355353f4cc27d02c3e
ry <yeryanyin@gmail.com>
2026-02-13 08:56:22 -0500

    Can Geode update?

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ number = random.randint(0,50)
 guess = int(input("Guess number between 0 and 50"))
 guess_history = " "
 
+def guess_history():
+    return guess
+
 while guess != number:
     if guess > number:
         print(f"The number is lower than {guess}")
-        guess_history = guess_history =+ guess,
+        guess_history =+ guess
         guess = int(input("Guess again from 0 to 50"))
     if guess < number:
         print(f"The number is greater than {guess}")
-        guess_history = guess_history =+ guess,
+        guess_history =+ guess
         guess = int(input("Guess again from 0 to 50"))
+
 print("You were correct!")
 print(guess_history)
